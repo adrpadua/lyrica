4765630409f434fb67c5d4f748470af66ceaf2ac
badixon <brent7890@gmail.com>
2018-02-03 06:54:53 -0800

    Changed rapMachine to make rhyme and triggers generation work with front end.

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 $(document).on("pagebeforeshow", "#page2", function(event){
-	firstWord = corpus.randomWord();
-	secondWord = corpus.randomWord();
+	firstWord = rapMachine.get_next_rhyme(1);
+	secondWord = rapMachine.get_next_rhyme(0);
 	document.getElementById("firstWord").innerHTML = firstWord;
 	document.getElementById("secondWord").innerHTML = secondWord;
 });
