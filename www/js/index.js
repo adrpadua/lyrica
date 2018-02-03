11503e11e0f32cc87b305a565fa21b64019693a4
adrpadua <adrpadua@gmail.com>
2018-02-03 10:34:32 -0800

    separated js and cleaned HTML

--- www/js/index.js
+++ www/js/index.js
@@ -5,9 +5,28 @@ $(document).on("pagebeforeshow", "#page2", function(event){
 	document.getElementById("secondWord").innerHTML = secondWord;
 });
 
-$("#getRhyme").click(function randomWords(){
-	firstWord = corpus.randomWord();
-	secondWord = corpus.randomWord();
+function rhyme() {
+	console.log("hi");
+
+	firstWord = rapMachine.get_next_rhyme(1);
+	secondWord = rapMachine.get_next_rhyme(0);
+	
+	refreshWords();
+}
+
+function trigger() {
+	firstWord = rapMachine.get_next_trigger();
+	secondWord = rapMachine.get_next_rhyme(0);
+	
+	refreshWords();
+}
+
+function refreshWords() {
+	$('.word-container').fadeOut(100, fadeWordsIn);
+}
+
+function fadeWordsIn() {
 	document.getElementById("firstWord").innerHTML = firstWord;
 	document.getElementById("secondWord").innerHTML = secondWord;
-});
+	$('.word-container').fadeIn(50)
+}
